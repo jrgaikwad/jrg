96a91772f4617748f9f9e5132952684402ff89ad
jrgaikwad <gaikwadjeevan73@gmail.com'>
2022-04-03 01:08:00 +0530

    fourth commit

--- src/assertions/HardAssert1.java
+++ src/assertions/HardAssert1.java
@@ -11,7 +11,7 @@ public class HardAssert1 {
 	  String c = "dinesh";
 	  Assert.assertEquals(d, c);
 	  System.out.println("After First Assertion");
-	  System.out.println("hi");
+	  System.out.println("hi hhh");
  
   
   }
